8ecb218cbfd9ccc08616d392ebf83a076e2e5f50
Kimtai Lewis <lewis.kimtai@gmail.com>
2024-08-04 14:54:06 +0300

    port

--- api/main.py
+++ api/main.py
@@ -51,4 +51,6 @@ async def predict(
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host='0.0.0.0', port=port)
